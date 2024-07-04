55e68a712ddd3bcc21002a3c280247fe8ee5a698
Rakesh Kumar <rklsspty777@gmail.com>
2024-07-04 14:13:17 +0530

    update

--- Basic-Concepts/1_exercises/2_nBooleanCombo.c
+++ Basic-Concepts/1_exercises/2_nBooleanCombo.c
@@ -6,6 +6,7 @@ typedef enum {
 } boolean;
 
 void printCombos(int depth, boolean* pStates, boolean currState, int size);
+void printCombosV2(int depth, boolean* pStates, int size);
 void booleanToString(boolean);
 
 int main() {
@@ -35,6 +36,23 @@ void printCombos(int depth, boolean* pStates, boolean currState, int size) {
         if(depth != 0)
             printCombos(depth - 1, pStates, false, size);
     }
+}
+
+// another version (fast)
+void printCombosV2(int depth, boolean* pStates, int size) {
+    if (depth == 0) {
+        for (int i = 0; i < size; i++) {
+            booleanToString(pStates[i]);
+        }
+        printf("\n");
+        return;
+    }
+
+    pStates[size - depth] = true;
+    printCombosV2(depth - 1, pStates, size);
+
+    pStates[size - depth] = false;
+    printCombosV2(depth - 1, pStates, size);
 }
 
 void booleanToString(boolean value) {
